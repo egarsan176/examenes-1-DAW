f6a0c136f8f5a66ec21be782be7c5b1ea90cdbec
egarsan176 <estefaniagarci@gmail.com>
2021-02-08 20:06:12 +0100

    Añadido examenes GIT y SVN a la carpeta ENTORNOS

---
+++ ENTORNOS/SVN/Contenedor.java
@@ -0,0 +1,56 @@
+package prueba;
+
+public class Contenedor implements Comparable<Contenedor>{
+	private String origen;
+	private String mercancia;
+	private float peso;
+	private String compañia;
+	
+	public Contenedor(String origen, String mercancia, float peso, String compañia) {
+		this.origen = origen;
+		this.mercancia = mercancia;
+		this.peso = peso;
+		this.compañia = compañia;
+	}
+
+	public String getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+
+	public String getMercancia() {
+		return mercancia;
+	}
+
+	public void setMercancia(String mercancia) {
+		this.mercancia = mercancia;
+	}
+
+	public float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+
+	public String getCompañia() {
+		return compañia;
+	}
+
+	public void setCompañia(String compañia) {
+		this.compañia = compañia;
+	}
+	
+	@Override
+	public int compareTo(Contenedor o) {
+		return Float.compare(peso, o.peso);
+	}
+	
+	
+
+}
+
